70368241aa40bc071a9d2237dd638b507e2d370d
mgmgchitwinoo <tonystark61609@gmail.com>
2025-01-29 15:14:41 +0700

    Example.php

--- Example.php
+++ Example.php
@@ -1,10 +1,21 @@
 <?php
 use Namdevel\Codashop;
 
-require(__DIR__ . '/vendor/autoload.php');
+require __DIR__ . '/vendor/autoload.php';
+                                                            header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
 $app = new Codashop();
 
-echo $app->gameList();
+$userID = $_POST['userID'] ?? '';
+$zoneID = $_POST['zoneID'] ?? '';
 
-echo $app->checkUser('MOBILE_LEGENDS', 161307989, 2244);
+if(empty($userID) || empty($zoneID)) {
+    echo json_encode(["error" => "User ID & Zone ID are req>
+    exit;
+}
+
+$response = $app->checkUser("MOBA", $userID, $zoneID);
+
+echo json_encode(["result" => $response]);
+?>
